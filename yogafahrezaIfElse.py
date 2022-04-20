f6ed8517490ecf87eba691559d75bf356576b6fe
YogaFahreza <104059646+YogaFahreza@users.noreply.github.com>
2022-04-20 14:33:01 +0700

    Add files via upload

---
+++ yogafahrezaIfElse.py
@@ -0,0 +1,45 @@
+#Yoga Fahreza
+#191011401954
+#06TPLM002
+#UTS KECERDASAN BUATAN
+
+
+#kondisi IF
+nilai = 9
+
+#jika kondisi benar/TRUE maka program akan mengeksekusi perintah dibawahnya
+if(nilai > 7):
+    print("Sembilan Lebih Besar Dari Angka Tujuh") # Kondisi Benar, Dieksekusi
+
+#jika kondisi salah/FALSE maka program tidak akan mengeksekusi perintah dibawahnya
+if(nilai > 10):
+    print("Sembilan Lebih Besar Dari Angka Sepuluh") # Kondisi Salah, Maka tidak tereksekusi
+
+
+#kondisi IF ELSE
+nilai = 3
+#Jika pernyataan pada if bernilai TRUE maka if akan dieksekusi, tetapi jika FALSE kode pada else yang akan dieksekusi.
+if(nilai > 7):
+    print("Selamat Anda Lulus")
+else:
+    print("Maaf Anda Tidak Lulus")
+
+
+#kondisi ELIF
+hari_ini = "Minggu"
+
+if(hari_ini == "Senin"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Selasa"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Rabu"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Kamis"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Jumat"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Sabtu"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Minggu"):
+    print("Saya akan libur")
+
